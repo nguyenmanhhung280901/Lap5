463011953922b1f5a25184781dd73b2198ca6966
nguyenmanhhung280901 <nguyenmanhhung280901@gmail.com>
2021-06-16 15:07:04 +0700

    Update Lap5

---
+++ Lap5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lap5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var highSchoolTeacher = new HighSchoolTeacher()
+            {
+                IdentityNumber = "ID001",
+                    Name = "Strong Hero",
+                    BaseSalary = 100,
+                    Level = 1,
+                    Bonus = 1,
+                    IsSenior = false,
+                    HighSchoolCode = "HS001"
+
+                };
+                Console.WriteLine(highSchoolTeacher.CalculateSalary());
+                var universityTeacher = new UniversityTeacher
+                {
+                IdentityNumber = "ID002",
+                Name = "Strong",
+                BaseSalary = 120,
+                Level = 2,
+                Bonus = 9000,
+                IsSenior = true,
+                EnglishLevel = 2,
+                UniversityCode = "UN001"
+            };
+            Console.WriteLine(highSchoolTeacher.CalculateSalary());
+            
+        }
+    }
+}
---
+++ Lap5/UniversityTeacher.cs
@@ -0,0 +1,24 @@
+﻿namespace Lap5
+{
+    public class UniversityTeacher: AbstractTeacher
+    {
+        public string UniversityCode { get; set; }
+        public int EnglishLevel { get; set; }
+
+        public override double CalculateSalary()
+        {
+            if (Bonus > 10000)
+            {
+                throw new AmountException("Bonus is more than 10,000", Name);
+
+            }
+            var salary = BaseSalary * Level + Bonus;
+            if (IsSenior && salary < 60000)
+            {
+                throw new AmountException("Senior Lecturer gets less than 60,000 salary", IdentityNumber);
+            }
+
+            return salary;
+        }
+    }
+}
